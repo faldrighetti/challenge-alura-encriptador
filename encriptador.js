eccf4bfd6ffa4ba311900349a5d8492a93bd2acf
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2022-08-23 01:14:59 -0300

    Mejora lo visual e inicia JS

--- encriptador.js
+++ encriptador.js
@@ -32,21 +32,34 @@ $botonCopiar = document.querySelector('#boton-copiar')
 
 const cambios = [['a', 'ai'], ['e', 'enter'], ['i','imes'], ['o','ober'], ['u', 'ufat']]
 
-let textoIngresado = document.querySelector('#campo-texto').value
+let textoIngresado = document.querySelector('#campo-texto')
+
+let cajaMunieco = document.querySelector('.caja-muneco')
+let advertencia = document.querySelector('.advertencia')
+let textoInstruccion = document.querySelector('.texto-instruccion')
+let campoCopiar = document.querySelector('#copiar')
 
 let resultado = document.querySelector('#resultado')
 
-/*$botonEncriptar.onclick = function() {
-    let textoEncriptado = encriptar(textoIngresado)
-    resultado.value(textoEncriptado)
-    textoIngresado.value = ''
+$botonEncriptar.onclick = function() {
+    let textoEncriptado = encriptar(textoIngresado.value)
+    resultado.textContent = textoEncriptado
+    resultado.className = ''
+    campoCopiar.className = ''
+    cajaMunieco.className = 'oculto'
+    advertencia.className = 'oculto'
+    textoInstruccion.className = 'oculto'
+}
+
+/*$botonCopiar.onclick = function(){
+    copiar(textoEncriptado)
 }*/
 
 function encriptar(frase){
     frase = frase.toLowerCase()
     for (let i = 0; i < cambios.length; i++){
         if(frase.includes(cambios[i][0])){
-            frase = frase.replace(cambios[i][0], cambios[i][1])
+            frase = frase.replaceAll(cambios[i][0], cambios[i][1])
         }
     }
     let textoEncriptado = frase    
@@ -57,7 +70,7 @@ function desencriptar(frase){
     frase = frase.toLowerCase()
     for (let i = 0; i < cambios.length; i++){
         if(frase.includes(cambios[i][1])){
-            frase = frase.replace(cambios[i][1], cambios[i][0])
+            frase = frase.replaceAll(cambios[i][1], cambios[i][0])
         }
     }
     let textoDesencriptado = frase
